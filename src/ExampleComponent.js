db853ddc925a8dade63d6af5063cd88a2276129e
mkanenobu <kaneda.1018@gmail.com>
2021-05-09 21:44:32 +0900

    chore

---
+++ src/ExampleComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./example_component.css";
+
+export const ExampleComponent = () => (
+  <div className={"container"}>
+    <p className={"text"}>This is ExampleComponent.</p>
+  </div>
+);
